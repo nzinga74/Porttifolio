4d108d6c1cc005d656cfe56ea9b5edc3968d0c2f
nzinga75 <nzingaenoque@gmail.com>
2022-02-21 00:35:24 +0100

    adding some carrousel in website

--- src/Pages/Home/Sections/Project/index.tsx
+++ src/Pages/Home/Sections/Project/index.tsx
@@ -16,7 +16,8 @@ import {
   faCode,
 } from "@fortawesome/free-solid-svg-icons";
 import Chip from "../../../../components/Chip";
-
+import Slider from "react-slick";
+import { Carrouselsettings } from "../../../../constants";
 const Project: React.FC = () => {
   return (
     <>
@@ -24,35 +25,83 @@ const Project: React.FC = () => {
         <Container>
           <ContentSection>
             <Title>Projetos Desenvolvidos</Title>
-            <ContentView>
+            <Slider {...Carrouselsettings}>
               <div>
-                <StarView>
-                  {/* <FontAwesomeIcon icon={faStar} color="#65f4aa" />
-                  <FontAwesomeIcon icon={faStar} color="#65f4aa" />
-                  <FontAwesomeIcon icon={faStar} color="#65f4aa" />
-                  <FontAwesomeIcon icon={faStar} color="#65f4aa" />
-                  <FontAwesomeIcon icon={faStar} color="#65f4aa" /> */}
-
-                  <Chip text="TS" />
-                  <Chip text="RN" />
-                </StarView>
-                <ContentText>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Est,
-                  fuga voluptatibus! At perspiciatis aut cupiditate, sequi iure
-                  minima quasi reiciendis ut dolores debitis ratione, quaerat
-                  facere ipsum itaque iusto perferendis!
-                </ContentText>
+                <ContentView>
+                  <div>
+                    <StarView>
+                      <Chip text="TS" />
+                      <Chip text="RN" />
+                    </StarView>
+                    <ContentText>
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Est, fuga voluptatibus! At perspiciatis aut cupiditate,
+                      sequi iure minima quasi reiciendis ut dolores debitis
+                      ratione, quaerat facere ipsum itaque iusto perferendis!
+                    </ContentText>
+                  </div>
+                  <ContentIconView>
+                    <FontAwesomeIcon
+                      icon={faCode}
+                      size={"3x"}
+                      width={100}
+                      color="#7c7d81"
+                      height={100}
+                    />
+                  </ContentIconView>
+                </ContentView>
               </div>
-              <ContentIconView>
-                <FontAwesomeIcon
-                  icon={faCode}
-                  size={"3x"}
-                  width={100}
-                  color="#7c7d81"
-                  height={100}
-                />
-              </ContentIconView>
-            </ContentView>
+              <div>
+                <ContentView>
+                  <div>
+                    <StarView>
+                      <Chip text="TS" />
+                      <Chip text="RN" />
+                    </StarView>
+                    <ContentText>
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Est, fuga voluptatibus! At perspiciatis aut cupiditate,
+                      sequi iure minima quasi reiciendis ut dolores debitis
+                      ratione, quaerat facere ipsum itaque iusto perferendis!
+                    </ContentText>
+                  </div>
+                  <ContentIconView>
+                    <FontAwesomeIcon
+                      icon={faCode}
+                      size={"3x"}
+                      width={100}
+                      color="#7c7d81"
+                      height={100}
+                    />
+                  </ContentIconView>
+                </ContentView>
+              </div>
+              <div>
+                <ContentView>
+                  <div>
+                    <StarView>
+                      <Chip text="TS" />
+                      <Chip text="RN" />
+                    </StarView>
+                    <ContentText>
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Est, fuga voluptatibus! At perspiciatis aut cupiditate,
+                      sequi iure minima quasi reiciendis ut dolores debitis
+                      ratione, quaerat facere ipsum itaque iusto perferendis!
+                    </ContentText>
+                  </div>
+                  <ContentIconView>
+                    <FontAwesomeIcon
+                      icon={faCode}
+                      size={"3x"}
+                      width={100}
+                      color="#7c7d81"
+                      height={100}
+                    />
+                  </ContentIconView>
+                </ContentView>
+              </div>
+            </Slider>
           </ContentSection>
         </Container>
       </CompanySection>
---
+++ src/constants/index.ts
@@ -0,0 +1,8 @@
+export const Carrouselsettings = {
+  dots: true,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+};
