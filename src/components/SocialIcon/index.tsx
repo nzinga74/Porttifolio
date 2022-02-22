52682623ec421347005dbe7190d3819f1f495fa0
nzinga75 <nzingaenoque@gmail.com>
2022-02-22 18:43:18 +0100

    icons

--- src/components/SocialIcon/index.tsx
+++ src/components/SocialIcon/index.tsx
@@ -3,11 +3,12 @@ import { StarView } from "./styles";
 // import { Container } from './styles';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHome } from "@fortawesome/free-solid-svg-icons";
+
 const SocialIcon: React.FC = () => {
   return (
     <StarView>
-      <FontAwesomeIcon icon={faHome} />
-      <FontAwesomeIcon icon={faHome} />
+      <FontAwesomeIcon icon={["fas", "coffee"]} />
+      <FontAwesomeIcon icon="check-square" />
       <FontAwesomeIcon icon={faHome} />
     </StarView>
   );
